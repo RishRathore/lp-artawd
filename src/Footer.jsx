b5735fd2297d71a9f506a996ea79578fbb799e70
rishabh <rishabhrathoretks@gmail.com>
2021-05-09 21:34:10 +0530

    create heade + banner + footer

---
+++ src/Footer.jsx
@@ -0,0 +1,25 @@
+const Footer = () => (
+  <footer className="footer bg-dark py-4 text-light">
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <p className="small text-center">
+            جميع الحقوق محفوظة 2020
+                    </p>
+          <br />
+          {/* <a target="_blank" href="./privacy.html">Privacy policy</a> _
+                    <a target="_blank" href="./terms.html" >Terms of use </a> _
+                    <a target="_blank" href="./disclaimer.html" >Disclaimer </a> */}
+          {/* <br />
+          <br /> */}
+          <hr />
+          <p style={{ fontSize:'11px', color: '#8f8d8d' }}>كوف انفيست ليس موقع للتداول<br />
+            ينطوي التداول على مخاطرة كبيرة ، والتي تنطوي على مستوى مرتفع من المخاطرة وغير مناسبة لكل مستثمر. قبل اتخاذ أي قرار ، يجب أن تفكر جيدًا في أهدافك ووضعك المالي واحتياجاتك ومستوى خبرتك ، وأن تفكر في طلب المشورة المهنية المستقلة
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
---
+++ src/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'
+import Footer from './Footer'
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default MainPage
